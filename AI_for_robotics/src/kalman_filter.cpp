1c7c6689776c4b6a5a02b5fdd737ab90321b32e6
RoboEvangelist <elpot_15@hotmail.com>
2019-12-06 18:36:05 -0800

    adding 1D kalman filter sample

--- AI_for_robotics/src/kalman_filter.cpp
+++ AI_for_robotics/src/kalman_filter.cpp
@@ -9,7 +9,7 @@ std::vector<float> KalmanFilter::kalman_update(float mean1, float var1,
 
 std::vector<float> KalmanFilter::kalman_predict(float mean1, float var1,
   float mean2, float var2) {
-  float new_mean = (var2 * mean1 + var1 * mean2) / (var1 + var2);
-  float new_var = 1.0 / ((1.0 / var1) + (1.0 / var2));
+  float new_mean = mean1 + mean2;
+  float new_var = var1 + var2;
   return {new_mean, new_var};
 }
--- AI_for_robotics/src/robot_main.cpp
+++ AI_for_robotics/src/robot_main.cpp
@@ -1,3 +1,8 @@
+#include <cassert>
+#include <chrono>
+#include <iomanip>
+#include <bits/stdc++.h>
+
 #include "robot/robot.hpp"
 #include "robot/kalman_filter.hpp"
 
@@ -19,11 +24,51 @@ int main(int argc, char** argv) {
     std::cout << measurement << ", ";
   std::cout << "\n";
 
-  std::vector<float> kalman_update =
-    kalman_filter.kalman_update(10, 8, 13, 2);
-  for (const auto update : kalman_update)
-    std::cout << update << ", ";
-  std::cout << "\n";
+  /// Kalman Filter
+  std::vector<float> kal_measurements = {5.0, 6.0, 7.0, 9.0, 10.0};
+  std::vector<float> motion = {1.0, 1.0, 2.0, 1.0, 1.0};
+
+  uint8_t motion_size = motion.size();
+
+  assert(kal_measurements.size() == motion_size);
+  
+  /// measurement uncertainty
+  constexpr float measurement_sig = 4;
+  /// motion uncertainty
+  constexpr float motion_sig = 2;
+  /// initial estimante. Basically the middle of the bell
+  float mu = 0;
+  /// uncertainty. How spread the curve is
+  float sig = 1000;
+
+//  auto start = std::chrono::high_resolution_clock::now(); 
+  // unsync the I/O of C and C++. 
+//  std::ios_base::sync_with_stdio(false);
+
+  /// position estimate
+  for (uint8_t i = 0; i < motion_size; ++i) {
+    std::vector<float> kalman_values =
+    kalman_filter.kalman_update(mu, sig, kal_measurements.at(i),
+      measurement_sig);
+    mu = std::move(kalman_values.at(0));
+    sig = std::move(kalman_values.at(1));
+    std::cout << "update: " << "[" << mu << ", " << sig << "]\n";
+    kalman_values =
+    kalman_filter.kalman_predict(mu, sig, motion.at(i), motion_sig);
+    mu = std::move(kalman_values.at(0));
+    sig = std::move(kalman_values.at(1));
+    std::cout << "predict " << "[" << mu << ", " << sig << "]\n";
+  }
+//  auto end = std::chrono::high_resolution_clock::now(); 
+//  // Calculating total time taken by the program. 
+//  double time_taken =  
+//    std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count(); 
+//  
+//  time_taken *= 1e-9; 
+//  
+//  std::cout << "Time taken by program is : " << std::fixed  
+//       << time_taken << std::setprecision(20); 
+//  std::cout << " sec" << std::endl;
 
   return 0;
 }
